90d577d4dfa50c8f944f8b4da1504d6b45939176
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-09-15 15:00:55 +0530

    complete profile routes added with middlewares

--- routes/litigantRoutes.js
+++ routes/litigantRoutes.js
@@ -1,23 +1,25 @@
 
 const express = require('express');
 const router = express.Router();
-const { signup, completeProfile, authenticate, getLitigants, getLitigant } = require('../controllers/litigantController');
+const { signup, completeProfile, authenticate,  getLitigant } = require('../controllers/litigantController');
 const { validateSignup, validateProfile } = require('../middlewares/validationMiddleware');
 const authenticateJWT = require('../middlewares/authmiddleware');
+const  uploadFields  = require('../middlewares/uploadMiddleware');
+
 
 // Signup route
 router.post('/signup', validateSignup, signup);
 
 // Complete profile route
-router.put('/profile', authenticateJWT, validateProfile, completeProfile); // Assuming user is authenticated
+router.post("/completeProfile", authenticateJWT,validateProfile, uploadFields, completeProfile); // Assuming user is authenticated
 
 // Authentication route
 router.post('/authenticate', authenticate);
 
 // Get all litigants route
-router.get('/litigants', authenticateJWT, getLitigants); // Assuming authentication required
+// router.get('/litigants', authenticateJWT, getLitigants); // Assuming authentication required
 
 // Get specific litigant route
-router.get('/litigant/:id?', authenticateJWT, getLitigant); // Either ID or email is required
+router.get('/getDetails/', authenticateJWT, getLitigant); // Either ID or email is required
 
 module.exports = router;
