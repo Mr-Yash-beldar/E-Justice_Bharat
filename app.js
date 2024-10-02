13e0be10eb0dbe65793ae04bfd0908e3affba204
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-10-02 10:13:03 +0530

    added case routes and cors for frontend

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const { connectDB, disconnectDB } = require('./config/db');  //database connecti
 const litigantRoutes = require('./routes/litigantRoutes'); // routes for litigants
 const otpRoutes = require('./routes/otpRoutes'); // routes for otp
 const meetingRoutes = require("./routes/meetingRoutes"); //routes for meeting
-
+const caseRoutes = require('./routes/caseRoutes'); // routes for cases
+const authRoutes = require('./routes/authRoutes'); // routes for authentication
+const cors = require('cors'); // Import cors
 const app = express();
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
 
@@ -15,10 +17,19 @@ app.use(express.json()); // Parse JSON bodies
 // Connect to database ejusticeBharat
 connectDB();
 
+app.use(cors({
+  origin: 'http://localhost:5173', // Allow specific frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Define allowed methods
+  credentials: true, // Allow cookies if needed
+}));
+
+
 // routes and middleware
 app.use('/litigants', litigantRoutes); // Use litigant routes
 app.use('/email', otpRoutes); // Use otp routes
 app.use("/ejusticeBharat/meeting", meetingRoutes); // Use meeting routes
+app.use('/cases', caseRoutes); // Use case routes
+app.use('/auth', authRoutes);
 
 // for handling the error and sending the response to the client 
 process.on('SIGINT', async () => {
