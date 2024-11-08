219b82d3059a374a44ed1397606e00c4a765fa7d
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-08 15:38:00 +0530

    distance calculation fixed

--- utils/GeoLocation/CalculateDistance.js
+++ utils/GeoLocation/CalculateDistance.js
@@ -1,6 +1,7 @@
 
 // Helper function to calculate distance between two geo-coordinates
 function calculateDistance(lat1, lon1, lat2, lon2) {
+    
     const R = 6371; // Radius of the Earth in kilometers
     const dLat = (lat2 - lat1) * Math.PI / 180;
     const dLon = (lon2 - lon1) * Math.PI / 180;
