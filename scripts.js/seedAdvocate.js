18e6c01d034444554b3d48fb1bcf1e186607cce3
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-12 08:19:33 +0530

    seeding advocate script

--- scripts.js/seedAdvocate.js
+++ scripts.js/seedAdvocate.js
@@ -2,9 +2,6 @@ const faker = require("faker");
 const Advocate = require("../models/advocate");
 const { connectDB, disconnectDB } = require('../config/db'); // Adjust the path according to your project structure
 
-// MongoDB connection URI
-const mongoURI = "your_mongodb_connection_uri_here"; // Replace with your actual MongoDB URI
-
 // Function to seed advocates
 const seedAdvocates = async () => {
   try {
@@ -14,7 +11,7 @@ const seedAdvocates = async () => {
     console.log("Connected to MongoDB...");
 
     // Generate advocate data
-    const advocates = Array.from({ length: 20 }, () => ({
+    const advocates = Array.from({ length: 1 }, () => ({
       fullName: faker.name.findName(),
       email: faker.internet.email().toLowerCase(),
       mobileNumber: faker.phone.phoneNumber('##########'), // 10 digits
@@ -22,26 +19,27 @@ const seedAdvocates = async () => {
       barLicenseNumber: faker.random.alphaNumeric(10), // Adjust based on your requirement
       dateOfBirth: faker.date.past(30, new Date('2000-01-01')), // Born at least 30 years ago
       specialization: [faker.random.arrayElement(['Criminal Law', 'Civil Law', 'Corporate Law', 'Family Law'])],
-      location: {
+      advocate_location: {
         type: 'Point',
         coordinates: [faker.address.longitude(), faker.address.latitude()],
       },  
       state: faker.address.state(),
       place: faker.address.city(),
-      digitalSignature: faker.image.imageUrl(), // Example URL for a digital signature
-      profileImage: faker.image.avatar(),
-      status: faker.random.arrayElement(['active', 'inactive', 'suspended']),
+      other_document: faker.image.imageUrl(), // Example URL for a digital signature
+      aadhar_document: faker.image.imageUrl(), // Example URL for a digital signature
+      profile_image: faker.image.avatar(),
     }));
 
     // Insert the advocates into the database
+    await Advocate.deleteMany({});
     await Advocate.insertMany(advocates);
 
-    console.log("20 Advocates have been seeded successfully!");
+    console.log("Advocates have been seeded successfully!");
   } catch (error) {
     console.error("Error seeding advocates:", error);
   } finally {
     // Close the connection
-    await mongoose.connection.close();
+    await disconnectDB();
   }
 };
 
