8be18a68a80c1192df8839e2ed61a5b80838610e
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-12 08:18:48 +0530

    added advocate routes

---
+++ routes/advocateRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const advocateController = require("../controllers/advocateController");
+const verifyToken = require("../middlewares/authMiddleware"); // Middleware to verify JWT
+
+// Route for signing up a new advocate
+router.post("/signup", advocateController.signup);
+
+// Route for authenticating an advocate and generating JWT
+router.post("/authenticate", advocateController.authenticate);
+
+// Route for completing advocate profile (requires authentication)
+router.put("/complete-profile", verifyToken, advocateController.completeProfile);
+
+// Route for getting a list of all advocates
+router.get("/getDetails", verifyToken,advocateController.getAdvocate);
+
+module.exports = router;
