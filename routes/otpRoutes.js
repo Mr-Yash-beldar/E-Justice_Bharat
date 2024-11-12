b7a4ed26ed9e81116fc6a63277a43296e7ceaddc
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-12 08:19:22 +0530

    added advocate otp routes

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { sendOtp, verifyOtp } = require('../controllers/otpController');
+const { sendOtp, verifyOtp,sendAdvocateOtp,verifyAdvocateOtp } = require('../controllers/otpController');
 
 // Route to send OTP via encrypted user ID
 router.get('/sendOtp', sendOtp);
 
+router.get('/sendAdvocateOtp', sendAdvocateOtp);
+
 // Route to verify OTP
 router.post('/verifyOtp', verifyOtp);
+
+router.post('/verifyAdvocateOtp', verifyAdvocateOtp);
 
 module.exports = router;
