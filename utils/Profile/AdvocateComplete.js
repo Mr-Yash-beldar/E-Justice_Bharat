651ec140b72192ca96dc32545e3604848c4f3405
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-12 08:20:07 +0530

    advocate complete profile stuff

---
+++ utils/Profile/AdvocateComplete.js
@@ -0,0 +1,30 @@
+// services/profileService.js
+
+const calculateAdvocateProfileCompletion = (advocate) => {
+    const requiredFields = [
+        'fullName',
+        'email',
+        'mobileNumber',
+        'password',
+        'barLicenseNumber',
+        'dateOfBirth',
+        'specialization',
+        'state',
+        'location',
+        'place',
+        'aadhar_document',
+        'other_document',
+        'preferred_language',
+        'pincode',
+    ];
+
+    const missingFields = requiredFields.filter(field => !advocate[field]);
+    const completionPercentage = ((requiredFields.length - missingFields.length) / requiredFields.length) * 100;
+  
+    return {
+      completionPercentage,
+      missingFields
+    };
+};
+
+module.exports = calculateAdvocateProfileCompletion;
