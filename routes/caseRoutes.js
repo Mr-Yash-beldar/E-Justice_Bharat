166c28aaa33f64d3df64cbf2a700a23283423bf6
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-14 09:48:31 +0530

    case verify user updated

--- routes/caseRoutes.js
+++ routes/caseRoutes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const caseController = require('../controllers/caseController');
-const authenticateLitigant = require('../middlewares/authMiddleware'); // Import authentication middleware
+const verifyAuthToken = require('../middlewares/authMiddleware'); // Import authentication middleware
 
 // Route for adding a new case
-router.post('/add', authenticateLitigant, caseController.addCase);
+router.post('/add', verifyAuthToken, caseController.addCase);
 
 // Route for updating a case
-router.put('/update/:caseId', authenticateLitigant, caseController.updateCase);
+router.put('/update/:caseId', verifyAuthToken, caseController.updateCase);
 
 // Route for deleting a case
-router.delete('/delete/:caseId', authenticateLitigant, caseController.deleteCase);
+router.delete('/delete/:caseId', verifyAuthToken, caseController.deleteCase);
 
 // Route for updating case status
-router.patch('/updateStatus/:caseId', authenticateLitigant, caseController.updateCaseStatus);
+router.patch('/updateStatus/:caseId', verifyAuthToken, caseController.updateCaseStatus);
 
 // Route for getting case details
-router.get('/:caseId', authenticateLitigant, caseController.getCaseDetails);
+router.get('/:caseId', verifyAuthToken, caseController.getCaseDetails);
 
 // Route for getting all cases by litigant
-router.get("/", authenticateLitigant, caseController.getAllCasesByLitigant);
+router.get("/", verifyAuthToken, caseController.getAllCasesByLitigant);
 
 module.exports = router;
