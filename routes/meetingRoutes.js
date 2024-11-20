8f1c4da876c0096c3d3d2dbc260b5c0b999408ae
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-11-20 16:47:37 +0530

    meeting routes added

--- routes/meetingRoutes.js
+++ routes/meetingRoutes.js
@@ -8,4 +8,11 @@ router.post("/create-meeting", videoController.createMeeting);
 // Route to validate a meeting by roomId
 router.get("/validate-meeting/:roomId", videoController.validateMeeting);
 
+//store 
+router.post("/store", videoController.storeMeeting);
+
+//getmeeting
+router.post("/get-meeting-code", videoController.getMeetingCodeByCaseId);
+
+
 module.exports = router;
