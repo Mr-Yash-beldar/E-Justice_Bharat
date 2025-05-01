4f8908f017d4afb075342ddf4688dae455d27756
Yashodip Beldar <yashodip2003beldar@gmail.com>
2025-05-01 20:51:14 +0530

    ready for deploy

--- app.js
+++ app.js
@@ -27,6 +27,18 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    return res.sendStatus(204);
+  }
+  next();
+});
+
+
 app.use(cors(corsOptions));
 
 // Explicitly handle OPTIONS requests for preflight
